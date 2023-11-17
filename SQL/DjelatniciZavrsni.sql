210aee87f3ae88fd35e68fe6c419780d70c4a6f0
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-11-17 09:58:46 +0100

    Kreirane tablice

--- SQL/DjelatniciZavrsni.sql
+++ SQL/DjelatniciZavrsni.sql
@@ -10,5 +10,22 @@ use djelatnici;
 create table fotografije(
 id int not null primary key identity(1,1),
 naziv varchar(100),
-lokacija 
+lokacija varchar(255),
+djelatnik int not null
+);
+
+create table dokumenti(
+id int not null primary key identity(1,1),
+naziv varchar(100),
+lokacija varchar(255),
+djelatnik int not null
+);
+
+create table djelatnici(
+id int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+kontakt varchar(150) not null,
+oib char(11) not null,
+iban varchar(50) not null
 );
