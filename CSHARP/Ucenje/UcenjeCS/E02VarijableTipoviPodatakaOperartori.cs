818f682764faf2c28508bf569a94ce133bd2855d
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-12-19 14:01:47 +0100

    ...

--- CSHARP/Ucenje/UcenjeCS/E02VarijableTipoviPodatakaOperartori.cs
+++ CSHARP/Ucenje/UcenjeCS/E02VarijableTipoviPodatakaOperartori.cs
@@ -11,60 +11,60 @@ namespace UcenjeCS
         public static void Izvedi()
         {
 
-            int Varijabla = 3;
-            Console.WriteLine(Varijabla);
+            //int Varijabla = 3;
+            //Console.WriteLine(Varijabla);
 
-            int i=1, j=0, k;
-            Console.WriteLine("{0},{1}",i,j);
+            int i=1, j=0, k=5;
+            Console.WriteLine($"{i},{j},{k}");
 
-            bool Istina = i == 1;
-            Console.WriteLine(Istina);
+            //bool Istina = i == 1;
+            //Console.WriteLine(Istina);
 
-            double Broj = 4.89;
-            decimal VeciBroj = 2.99M;
+            //double Broj = 4.89;
+            //decimal VeciBroj = 2.99M;
 
-            Console.WriteLine(int.MaxValue);
-            int MB = int.MaxValue;
-            Console.WriteLine(MB+1);
+            //Console.WriteLine(int.MaxValue);
+            //int MB = int.MaxValue;
+            //Console.WriteLine(MB+1);
 
-            i = 3; j = 2; k = 1;
-            Console.WriteLine(i+j);
-            Console.WriteLine(i-j);
-            Console.WriteLine(i*j);
-            Console.WriteLine(i/j);
-            Console.WriteLine((float)i/j);
+            //i = 3; j = 2; k = 1;
+            //Console.WriteLine(i+j);
+            //Console.WriteLine(i-j);
+            //Console.WriteLine(i*j);
+            //Console.WriteLine(i/j);
+            //Console.WriteLine((float)i/j);
 
-            // Ispiši prvu znamenku dvoznamenkastog broja
-            int DB = 56;
-            Console.WriteLine(DB/10);
+            //// Ispiši prvu znamenku dvoznamenkastog broja
+            //int DB = 56;
+            //Console.WriteLine(DB/10);
 
-            bool Uvjet = i > j;
-            Uvjet = i >= j;
-            Uvjet = i <= j;
-            Uvjet = i < j;
-            Uvjet = i == j;
-            Uvjet = i != j;
+            //bool Uvjet = i > j;
+            //Uvjet = i >= j;
+            //Uvjet = i <= j;
+            //Uvjet = i < j;
+            //Uvjet = i == j;
+            //Uvjet = i != j;
 
-            int Ostatak = 9 % 2;
-            Console.WriteLine(Ostatak);
+            //int Ostatak = 9 % 2;
+            //Console.WriteLine(Ostatak);
 
-            // Za dvoznamenkasti broj ispiši vrijednost jedinice
-            Console.WriteLine(52 % 10);
+            //// Za dvoznamenkasti broj ispiši vrijednost jedinice
+            //Console.WriteLine(52 % 10);
 
-            i = 1;
-            Console.WriteLine(i);//1
-            i = i + 1;
-            Console.WriteLine(i);//2
-            i += 1;
-            Console.WriteLine(i);//3
-            i++;
-            Console.WriteLine(i);//4
+            //i = 1;
+            //Console.WriteLine(i);//1
+            //i = i + 1;
+            //Console.WriteLine(i);//2
+            //i += 1;
+            //Console.WriteLine(i);//3
+            //i++;
+            //Console.WriteLine(i);//4
 
-            // Specifičnosti incrementa
-            Console.WriteLine(i++);//4
-            Console.WriteLine(i);//5
-            Console.WriteLine(++i);//6
-            Console.WriteLine(i);//6
+            //// Specifičnosti incrementa
+            //Console.WriteLine(i++);//4
+            //Console.WriteLine(i);//5
+            //Console.WriteLine(++i);//6
+            //Console.WriteLine(i);//6
 
             
 
