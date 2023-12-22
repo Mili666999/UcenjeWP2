ba402d5e46374575213e1b903485841e3868db17
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-12-22 11:35:28 +0100

    Započeta ciklična matrica

---
+++ CSHARP/Vjezbe/VjezbeCS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VjezbeCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            V01CiklicnaMatrica.Izvedi();
+        }
+    }
+}
---
+++ CSHARP/Vjezbe/VjezbeCS/V01CiklicnaMatrica.cs
@@ -0,0 +1,14 @@
+﻿namespace VjezbeCS
+{
+    internal class V01CiklicnaMatrica
+    {
+        public static void Izvedi()
+        { 
+            int i;
+            for (i = 10; i >= 1; i--)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
