1e28f84d9d69940de28e93a522f83dd98b2c946e
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-12-22 14:35:03 +0100

    ...

--- CSHARP/Vjezbe/VjezbeCS/V01CiklicnaMatrica.cs
+++ CSHARP/Vjezbe/VjezbeCS/V01CiklicnaMatrica.cs
@@ -3,12 +3,47 @@
     internal class V01CiklicnaMatrica
     {
         public static void Izvedi()
-        { 
-            int i;
-            for (i = 10; i >= 1; i--)
+        {
+            Console.Write("Unesi broj redova: ");
+            int red = int.Parse(Console.ReadLine());
+            
+            Console.Write("Unesi broj stupaca: ");
+            int stup = int.Parse(Console.ReadLine());
+            
+            int[,] Matrica = new int[red,stup];
+            
+            for(int i = 0; i < red; i++) 
             {
-                Console.Write(i + " ");
+                for (int j = 0; j < stup; j++)
+                {
+                    Console.Write(Matrica[i,j] + "\t");
+                }
+                Console.WriteLine();
             }
+            Console.WriteLine("*****************************************************************");
+            Matrica[red - 1, stup - 1] = 1;
+
+            for (int i = 0; i < red; i++)
+            {
+                for (int j = 0; j < stup; j++)
+                {
+                    Console.Write(Matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("*****************************************************************");
+            Matrica[red - 1, stup - 2] = 2;
+            for (int i = 0; i < red; i++)
+            {
+                for (int j = 0; j < stup; j++)
+                {
+                    Console.Write(Matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("*****************************************************************");
+
+
         }
     }
 }
