9f1cc2aa795213e0a6ac75430a2759eedf76938e
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-12-30 15:20:32 +0100

    Prvi krug

--- CSHARP/Vjezbe/VjezbeCS/V01CiklicnaMatrica.cs
+++ CSHARP/Vjezbe/VjezbeCS/V01CiklicnaMatrica.cs
@@ -9,19 +9,44 @@
             
             Console.Write("Unesi broj stupaca: ");
             int stup = int.Parse(Console.ReadLine());
+
+            int b = 1;
             
             int[,] Matrica = new int[red,stup];
+
+
+            //Matrica[red-1,stup-1] = 1;
             
-            for(int i = 0; i < red; i++) 
-            {
-                for (int j = 0; j < stup; j++)
-                {
-                    Console.Write(Matrica[i,j] + "\t");
-                }
-                Console.WriteLine();
-            }
+            //for(int i = 0; i < red ; i++) 
+            //{
+            //    for (int j = 0; j < stup; j++)
+            //    {
+            //        Console.Write((Matrica[i,j]) + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("*****************************************************************");
+            //Matrica[red - 1, stup - 2] = 2;
+
+            //for (int i = 0; i < red; i++)
+            //{
+            //    for (int j = 0; j < stup; j++)
+            //    {
+            //        Console.Write(Matrica[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
             Console.WriteLine("*****************************************************************");
-            Matrica[red - 1, stup - 1] = 1;
+            //Matrica[red - 1, stup - 1] = 1;
+            //Matrica[red - 1, stup - 2] = 2;
+            //Matrica[red - 1, stup - 3] = 3;
+            //Matrica[red - 1, stup - 4] = 4;
+            //Matrica[red - 1, stup - 5] = 5;
+
+            for (int i = 1; i <= stup; i++)
+            {
+                Matrica[red - 1, stup - i] = b++;
+            }
 
             for (int i = 0; i < red; i++)
             {
@@ -32,8 +57,36 @@
                 Console.WriteLine();
             }
             Console.WriteLine("*****************************************************************");
-            Matrica[red - 1, stup - 2] = 2;
-            for (int i = 0; i < red; i++)
+            //Matrica[red - 2, 0] = b++;
+            //Matrica[red - 3, 0] = b++;
+            //Matrica[red - 4, 0] = b++;
+            //Matrica[red - 5, 0] = b++;
+
+            for (int i = 2; i <= red; i++)
+            {
+                Matrica[red-i, 0] = b++;
+            }
+
+            for (int i=0; i<red; i++)
+            {
+                for (int j=0; j<stup; j++)
+                {
+                    Console.Write(Matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("*****************************************************************");
+            //Matrica[0, 1] = b++;
+            //Matrica[0, 2] = b++;
+            //Matrica[0, 3] = b++;
+            //Matrica[0, 4] = b++;
+
+            for (int i = 1; i < stup; i++)
+            {
+                Matrica[0,i] = b++;
+            }
+
+            for (int i=0; i < red; i++)
             {
                 for (int j = 0; j < stup; j++)
                 {
@@ -42,6 +95,23 @@
                 Console.WriteLine();
             }
             Console.WriteLine("*****************************************************************");
+            //Matrica[1,stup-1] = b++;
+            //Matrica[2,stup-1] = b++;
+            //Matrica[3,stup-1] = b++;
+
+            for (int i = 1; i < red-1; i++)
+            {
+                Matrica[i, stup-1] = b++;
+            }
+
+            for (int i = 0; i < red; i++)
+            {
+                for (int j = 0; j < stup; j++)
+                {
+                    Console.Write(Matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
 
         }
