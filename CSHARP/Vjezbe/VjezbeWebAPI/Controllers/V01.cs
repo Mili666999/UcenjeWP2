66f9d230b514eae028bcd2f5bd1a5dcd931632ec
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-12-30 19:43:25 +0100

    WebAPI/V01/Vjezba1

---
+++ CSHARP/Vjezbe/VjezbeWebAPI/Controllers/V01.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace VjezbeWebAPI.Controllers
+{
+    [ApiController]
+    [Route("V01")]
+    public class V01 : ControllerBase
+    {
+        [HttpGet]
+        [Route("Vjezba1")]
+
+        public float Vjz1(int A, int B, string Operator)
+        {
+            switch (Operator)
+            {
+                case "+":
+                    return A + B;
+                    break;
+                case "-":
+                    return A - B;
+                    break;
+                case "*":
+                    return A * B;
+                    break;
+                case "/":
+                    return (float)A / B;
+                    break;
+            }
+            return 0;
+        }
+    }
+}
