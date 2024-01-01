463420a4a84e078e59a7e969a44784b4716eada8
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-01 16:58:20 +0100

    Prvi pokušaj, potrebno doraditi

---
+++ CSHARP/Vjezbe/VjezbeWebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace VjezbeWebAPI.Controllers
+{
+    [ApiController]
+    [Route("V02")]
+    public class V02ZimskoVjezbanje : ControllerBase
+    {
+        [HttpGet]
+        [Route("Zadatak1")]
+
+        public int Zad1()
+        {
+            int Zbroj = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                Zbroj += i;
+            }
+            return Zbroj;
+        }
+
+        [HttpGet]
+        [Route("Zadatak2")]
+
+        public int[] Zad2() 
+        {
+            int[] Niz = new int[57];
+            for (int i = 1; i <= 57; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Niz[i] += i;
+                }
+            }
+
+            return Niz;
+        }
+    }
+}
