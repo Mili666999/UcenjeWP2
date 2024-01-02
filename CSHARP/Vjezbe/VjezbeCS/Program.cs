44692b61a786e7fdb7dbd4fe40ae42aa4b0a079e
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-02 10:57:47 +0100

    ...

--- CSHARP/Vjezbe/VjezbeCS/Program.cs
+++ CSHARP/Vjezbe/VjezbeCS/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            CiklicnaMatrica.Izvedi();
+            //CiklicnaMatrica.Izvedi();
+            //V01ZimskoVjezbanje.Ispis1Do100();
+            //V01ZimskoVjezbanje.Ispis100Do1();
+            //V01ZimskoVjezbanje.Ispis1Do100DjeljiviSa7();
+            V01ZimskoVjezbanje.VeciOd100();
         }
     }
 }
