3d3c5a362a92508707ad53adeb900e2305354c5d
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-02 10:58:22 +0100

    Riješeno

--- CSHARP/Vjezbe/VjezbeCS/V01ZimskoVjezbanje.cs
+++ CSHARP/Vjezbe/VjezbeCS/V01ZimskoVjezbanje.cs
@@ -8,5 +8,50 @@ namespace VjezbeCS
 {
     internal class V01ZimskoVjezbanje
     {
+        public static void Ispis1Do100()
+        { 
+            for (int i = 0; i < 100;  i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Ispis100Do1()
+        {
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Ispis1Do100DjeljiviSa7() 
+        {
+            for(int i = 1; i < 100; i++) 
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void VeciOd100()
+        {
+            int i = 0;
+            int j = 0;
+
+            Console.WriteLine("Pokušaj unjeti broj veći od 100");
+
+            for ( ; ; ) 
+            {
+                i = int.Parse(Console.ReadLine());
+                if (i > 100) 
+                {
+                    break;
+                }
+                j++;
+            }
+            Console.WriteLine("Unjeli ste broj iz " + j + " pokušaja!");
+        }
     }
 }
