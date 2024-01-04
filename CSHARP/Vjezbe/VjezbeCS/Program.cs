ac006e18dd8ea18a0b3af0d57eb0eea69bdf69d5
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-04 11:13:39 +0100

    Tablica množenja

--- CSHARP/Vjezbe/VjezbeCS/Program.cs
+++ CSHARP/Vjezbe/VjezbeCS/Program.cs
@@ -8,7 +8,8 @@
             //V01ZimskoVjezbanje.Ispis1Do100();
             //V01ZimskoVjezbanje.Ispis100Do1();
             //V01ZimskoVjezbanje.Ispis1Do100DjeljiviSa7();
-            V01ZimskoVjezbanje.VeciOd100();
+            //V01ZimskoVjezbanje.VeciOd100();
+            V01ZimskoVjezbanje.TablicaMnozenja();
         }
     }
 }
