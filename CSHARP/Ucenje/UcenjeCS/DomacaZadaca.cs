f7adeb345746404781e18f6ed4ff03903578060d
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-16 13:05:19 +0100

    ...

---
+++ CSHARP/Ucenje/UcenjeCS/DomacaZadaca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class DomacaZadaca
+    {
+
+        //Napisati metodu koja za dva primljena decimalna broja
+        //vraća cijeli dio zbroja primljenih brojeva
+       
+        public static int CijeliDioZbroja(float Prvi, float Drugi)
+        {
+            //return (int)(Prvi + Drugi);
+            return CijeliDioZbroja((double)Prvi, (double)Drugi);
+        }
+
+        //Method overloading
+        //Potpis metode: naziv + lista parametara
+
+        public static int CijeliDioZbroja(double Prvi, double Drugi)
+        { 
+            return (int) (Prvi + Drugi);    
+        }
+    }
+}
+
