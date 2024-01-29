a485cd3444127863f634adaea9b598526efd3942
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-01-29 11:14:46 +0100

    Kreiran model

---
+++ CSHARP/KonzolnaAPP/Model/Djelatnik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonzolnaAPP.Model
+{
+    internal class Djelatnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Kontakt { get; set; }
+        public string Oib { get; set; }
+        public string Iban { get; set; }
+    }
+}
---
+++ CSHARP/KonzolnaAPP/Model/Fotografija.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonzolnaAPP.Model
+{
+    internal class Fotografija
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Lokacija { get; set; }
+        public Djelatnik Djelatnik { get; set; }
+    }
+}
