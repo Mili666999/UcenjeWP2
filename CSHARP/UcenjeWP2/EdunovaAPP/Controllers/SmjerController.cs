6c463418953c1abfeec1a8c991ec7ebdfafeeaeb
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-02-15 21:45:05 +0100

    Predavanje 2024-02-13

---
+++ CSHARP/UcenjeWP2/EdunovaAPP/Controllers/SmjerController.cs
@@ -0,0 +1,65 @@
+﻿using EdunovaAPP.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdunovaAPP.Controllers
+{
+    /// <summary>
+    /// Namjenjeno za CRUD operacije nad entitetom Smjer u bazi
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SmjerController:ControllerBase
+    {
+        /// <summary>
+        /// Kontekst za rad s bazom koji će biti postavljen s pomoću Dependency Injection-om
+        /// </summary>
+        private readonly EdunovaContext _context;
+
+        /// <summary>
+        /// Konstruktor klase koja prima Edunova kontekst
+        /// pomoću DI principa
+        /// </summary>
+        /// <param name="context"></param>
+        public SmjerController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Dohvaća sve smjerove iz baze
+        /// </summary>
+        ///<remarks>
+        ///Primjer upita 
+        /// 
+        ///     GET api/v1/Smjer
+        ///     
+        /// </remarks>
+        /// <returns>Smjerovi u bazi</returns>
+        /// <response code="200">Sve OK</response>
+        /// <response code="400">Zahtjev nije valjan</response>
+        /// <response code="503">Baza na koju se spajam nije dostupna</response>
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            // kontrola ukoliko upit nije valjan
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var smjerovi = _context.Smjerovi.ToList();
+                if (smjerovi == null || smjerovi.Count == 0)
+                {
+                    return new EmptyResult();
+                }
+                return new JsonResult(smjerovi);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                    ex.Message);
+            }
+        }
+    }
+}
